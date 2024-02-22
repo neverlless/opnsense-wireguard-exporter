ec94a6f147d300e2a1d8903286a70c61bd7c6e5a
Maksim Hrytsaienko <m.hrytsaienko@sellerlogic.com>
2024-02-22 15:28:45 +0200

    Add environment variable support for client configuration

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"opnsense-wireguard-exporter/pkg/wireguard"
@@ -10,30 +11,37 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	// Настройка клиента
-	apiKey := ""                       // Замените на ваш API ключ
-	apiSecret := ""                    // Замените на ваш API секрет
-	baseURL := "https://192.168.2.254" // Замените на ваш базовый URL
+const (
+	defaultMetricsUpdateInterval = 30 * time.Second // Interval for metrics update
+	defaultListenAddress         = ":9100"          // Default listen address for the server
+)
 
+func main() {
+	// Client configuration
+	apiKey := os.Getenv("OPNSENSE_API_KEY")       // Replace with your API key from environment variable
+	apiSecret := os.Getenv("OPNSENSE_API_SECRET") // Replace with your API secret from environment variable
+	baseURL := os.Getenv("OPNSENSE_BASE_URL")     // Replace with your base URL from environment variable
+
+	// Create a new OPNsense client
 	client, err := wireguard.NewClient(apiKey, apiSecret, baseURL)
 	if err != nil {
 		log.Fatalf("Error creating OPNsense client: %v", err)
 	}
 
-	// Цикл обновления метрик
+	// Metrics update loop
 	go func() {
 		for {
 			if err := client.UpdateMetrics(); err != nil {
 				log.WithError(err).Error("Error updating metrics")
 			}
-			time.Sleep(30 * time.Second)
+			time.Sleep(defaultMetricsUpdateInterval)
 		}
 	}()
 
+	// Set up Prometheus metrics endpoint
 	http.Handle("/metrics", promhttp.Handler())
-	log.Info("Beginning to serve on port :9100")
-	if err := http.ListenAndServe(":9100", nil); err != nil {
+	log.Infof("Beginning to serve on port %s", defaultListenAddress)
+	if err := http.ListenAndServe(defaultListenAddress, nil); err != nil {
 		log.WithError(err).Fatal("Server failed")
 	}
 }
