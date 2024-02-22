067c2b92e834026ed6ebd64e39a82c23608c0878
Maksim Hrytsaienko <m.hrytsaienko@sellerlogic.com>
2024-02-22 14:18:04 +0200

    Add WireGuard metrics for peer status and total peers

--- pkg/wireguard/client.go
+++ pkg/wireguard/client.go
@@ -40,6 +40,19 @@ var (
 		Name: "opnsense_product_version_info",
 		Help: "The current version of the OPNsense product",
 	}, []string{"version"})
+	wgPeerLastHandshakeMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "wireguard_peer_last_handshake_seconds",
+		Help: "Last handshake time with the peer as UNIX timestamp.",
+	}, []string{"interface", "peer_name", "public_key"})
+
+	wgPeerStatusMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "wireguard_peer_status",
+		Help: "Status of the WireGuard peer (enabled/disabled).",
+	}, []string{"interface", "peer_name", "public_key"})
+	wgTotalPeersMetric = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "wireguard_total_peers",
+		Help: "Total number of WireGuard peers.",
+	})
 )
 
 func init() {
@@ -48,6 +61,9 @@ func init() {
 	prometheus.MustRegister(connectionStatusMetric)
 	prometheus.MustRegister(repositoryStatusMetric)
 	prometheus.MustRegister(productVersionMetric)
+	prometheus.MustRegister(wgPeerLastHandshakeMetric)
+	prometheus.MustRegister(wgPeerStatusMetric)
+	prometheus.MustRegister(wgTotalPeersMetric)
 
 	// Настройка logrus
 	log.SetFormatter(&log.TextFormatter{
@@ -73,7 +89,66 @@ func (c *Client) UpdateMetrics() error {
 
 	productVersionMetric.With(prometheus.Labels{"version": firmwareStatus.ProductVersion}).Set(1) // Установка версии продукта
 
+	wgStatus, err := c.FetchWireGuardStatus()
+	if err != nil {
+		return err
+	}
+
+	totalPeers := 0 // Initialize a counter for the total number of peers
+
+	for _, wg := range wgStatus.Items {
+		for _, peer := range wg.Peers {
+			if peer.LastHandshake != "0000-00-00 00:00:00+00:00" {
+				lastHandshakeTime, err := time.Parse("2006-01-02 15:04:05-07:00", peer.LastHandshake)
+				if err != nil {
+					log.WithError(err).Error("Failed to parse last handshake time")
+					continue
+				}
+				wgPeerLastHandshakeMetric.With(prometheus.Labels{
+					"interface":  wg.Interface,
+					"peer_name":  peer.Name,
+					"public_key": peer.PublicKey,
+				}).Set(float64(lastHandshakeTime.Unix()))
+			} else {
+				// Handle the case where the last handshake time is not a valid date.
+				// For example, you can set the metric to a default value like 0.
+				wgPeerLastHandshakeMetric.With(prometheus.Labels{
+					"interface":  wg.Interface,
+					"peer_name":  peer.Name,
+					"public_key": peer.PublicKey,
+				}).Set(0)
+			}
+
+			wgPeerStatusMetric.With(prometheus.Labels{
+				"interface":  wg.Interface,
+				"peer_name":  peer.Name,
+				"public_key": peer.PublicKey,
+			}).Set(float64(peer.Enabled))
+
+			totalPeers++
+		}
+	}
+
+	// Set the total number of peers metric
+	wgTotalPeersMetric.Set(float64(totalPeers))
+
 	return nil
+
+}
+
+// FetchWireGuardStatus fetches the WireGuard status from the endpoint
+func (c *Client) FetchWireGuardStatus() (*WireGuardStatus, error) {
+	body, err := c.fetch("/api/wireguard/general/getStatus")
+	if err != nil {
+		return nil, err
+	}
+
+	var status WireGuardStatus
+	if err := json.Unmarshal(body, &status); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal WireGuard status: %v", err)
+	}
+
+	return &status, nil
 }
 
 // NewClient создает новый клиент для OPNsense API
@@ -156,4 +231,19 @@ type FirmwareStatus struct {
 	ProductLatest      string `json:"product_latest"`
 	ProductVersion     string `json:"product_version"`
 	StatusMsg          string `json:"status_msg,omitempty"`
+}
+
+type WireGuardStatus struct {
+	Items map[string]struct {
+		Instance  int    `json:"instance"`
+		Interface string `json:"interface"`
+		Enabled   int    `json:"enabled"`
+		Name      string `json:"name"`
+		Peers     map[string]struct {
+			Name          string `json:"name"`
+			Enabled       int    `json:"enabled"`
+			PublicKey     string `json:"publicKey"`
+			LastHandshake string `json:"lastHandshake"`
+		} `json:"peers"`
+	} `json:"items"`
 }
